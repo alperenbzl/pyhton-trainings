5df2c99b3b25a1989a48782c7ff5d28c83f9c8a6
Başak ERDEM <basakavdanerdem@gmail.com>
2021-05-22 16:45:32 +0300

    changed variables

--- basak_math.py
+++ basak_math.py
@@ -1,16 +1,18 @@
-
 # toplama
 def Topla(degisken1, degisken2):
-    return degisken1 + degisken2
+    return int(degisken1) + int(degisken2)
+
 
 # cikarma
-def Cikar(degisken1,degisken2):
-    return degisken2 - degisken1
+def Cikar(degisken1, degisken2):
+    return int(degisken1) - int(degisken2)
+
 
 # carpma
-def Carpma(degisken1,degisken2):
-    return degisken1 * degisken2
+def Carpma(degisken1, degisken2):
+    return int(degisken1) * int(degisken2)
+
 
 # bolme
-def Bolme (degisken2,degisken1):
-    return degisken2 / degisken1
+def Bolme(degisken2, degisken1):
+    return int(degisken1) / int(degisken2)
--- basaktest.py
+++ basaktest.py
@@ -1,6 +1,12 @@
 import basak_math
 
 a = basak_math.Topla(3, 8)
-b = basak_math.Topla("3", "8")
+b = basak_math.Topla(int("3"), int("8"))
+c = basak_math.Cikar(10, 23)
+d = basak_math.Carpma(2, 4)
+e = basak_math.Bolme(8, 16)
 print(a)
 print(b)
+print(c)
+print(d)
+print(e)
