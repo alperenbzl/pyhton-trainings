80f1153db480d9c0eec34b47601964d1b2a2b24f
alperenbzl <erenbozali610@hotmail.com>
2021-05-22 16:34:08 +0300

    added libraries

---
+++ eren_math.py
@@ -0,0 +1,29 @@
+#toplama
+
+def topla (param1,param2):
+    return param1+param2
+
+
+#cıkarma 
+
+def cıkarma (s1,s2):
+    return s1-s2
+
+#carpma
+
+def carpma (p1,p2,p3):
+    return p1*p2*p3
+
+#bolme
+
+def bolme(k1,k2):
+    return k1/k2
+
+
+
+
+    
+
+
+
+
--- test.py
+++ test.py
@@ -1,1 +1,29 @@
-print("hello world")
+import eren_math
+
+a=eren_math.topla(56,876)
+print(a)
+
+c=eren_math.cıkarma(60,8)
+print(c)
+
+d=eren_math.carpma(10,5,3)
+print (d)
+
+e=eren_math.bolme(10,5)
+print(e)
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+ 
