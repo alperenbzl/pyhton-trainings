a289d07744c6643e72fee654873ddf3390e3f69b
Galip Tolga Erdem <galiperdem@gmail.com>
2021-05-22 16:56:29 +0300

    updated tests

--- basaktest.py
+++ basaktest.py
@@ -1,7 +1,7 @@
 import basak_math
 
 a = basak_math.Topla(3, 8)
-b = basak_math.Topla(int("3"), int("8"))
+b = basak_math.Topla("3", "8")
 c = basak_math.Cikar(10, 23)
 d = basak_math.Carpma(2, 4)
 e = basak_math.Bolme(8, 16)
@@ -10,3 +10,5 @@ print(b)
 print(c)
 print(d)
 print(e)
+
+num1 = basak_math.Bolme(22, 7)
--- test.py
+++ test.py
@@ -1,27 +1,22 @@
 import eren_math
 
-a=eren_math.topla(56,876)
+a = eren_math.topla(56, 876)
 print(a)
 
-c=eren_math.cıkarma(60,8)
+c = eren_math.cıkarma(60, 8)
 print(c)
 
-d=eren_math.carpma(10,5,3)
-print (d)
+d = eren_math.carpma(10, 5, 3)
+print(d)
 
-e=eren_math.bolme(10,5)
+e = eren_math.bolme(10, 5)
 print(e)
 
 
-a1 = eren_math.topla("1","2")
+a1 = eren_math.topla("1", "2")
 # a11 = eren_math.topla("a","b")
-a2=eren_math.cıkarma("1","2")
-a3=eren_math.bolme("1","2")
-a5=eren_math.carpma("1","2","3")
-  
+a2 = eren_math.cıkarma("1", "2")
+a3 = eren_math.bolme("1", "2")
+a5 = eren_math.carpma("1", "2", "3")
 
-
-
-
-
- 
+num1 = eren_math.bolme(22, 7)
