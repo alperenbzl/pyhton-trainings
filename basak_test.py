cf7191781a2da91fd5be6cd8a4af340e48750989
Galip Tolga Erdem <galiperdem@gmail.com>
2021-05-22 17:02:08 +0300

    updated with folders

---
+++ basak_test.py
@@ -0,0 +1,14 @@
+import basak.basak_math as bMath
+
+a = bMath.Topla(3, 8)
+b = bMath.Topla("3", "8")
+c = bMath.Cikar(10, 23)
+d = bMath.Carpma(2, 4)
+e = bMath.Bolme(8, 16)
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
+num1 = bMath.Bolme(22, 7)
---
+++ eren_test.py
@@ -0,0 +1,22 @@
+import eren.eren_math as eMath
+
+a = eMath.topla(56, 876)
+print(a)
+
+c = eMath.cıkarma(60, 8)
+print(c)
+
+d = eMath.carpma(10, 5, 3)
+print(d)
+
+e = eMath.bolme(10, 5)
+print(e)
+
+
+a1 = eMath.topla("1", "2")
+# a11 = eren_math.topla("a","b")
+a2 = eMath.cıkarma("1", "2")
+a3 = eMath.bolme("1", "2")
+a5 = eMath.carpma("1", "2", "3")
+
+num1 = eMath.bolme(22, 7)
