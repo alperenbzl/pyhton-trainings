d74c50e83cd010df54412cf520184a473c0dfcc0
Başak ERDEM <basakavdanerdem@gmail.com>
2021-05-22 16:11:04 +0300

    added basak_math

---
+++ basak_math.py
@@ -0,0 +1,12 @@
+# toplama
+def Topla(degisken1, degisken2):
+    return degisken1 + degisken2
+
+
+# cikarma
+
+
+# carpma
+
+
+# bolme
---
+++ main.py
@@ -0,0 +1,4 @@
+import basak_math
+
+sonuc = basak_math.Topla(3, 8)
+print(sonuc)
